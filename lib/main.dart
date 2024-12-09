5951fe59657fbd0fbcd1335a96d129c04046beea
Queue9669 <Queue9669@gmail.com>
2024-12-09 09:36:52 +0900

    3-3
    3-3

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,79 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MaterialApp(
+      home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var a=1;
+  var name = ['김영숙', '홍길동', '피자집'];
+  var like = [0, 0, 0];
+
   @override
   Widget build(BuildContext context) {
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('app'),),
-        body: Text('hi'),
-        bottomNavigationBar: BottomAppBar(
-            child: SizedBox(
-                height: 60,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Icon(Icons.phone),
-                    Icon(Icons.message),
-                    Icon(Icons.contact_page),
-                  ],
-                )
-            )
-        ),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){showDialog( context: context, builder: (context){
+            return DialogUI();
+          },
+          );}
+      ),
+      appBar: AppBar(title: Text('app'),),
+      body: ListView.builder(
+          itemCount: 3,
+          itemBuilder: (context, i) {
+            return ListTile(
+              leading: Image.asset('name'),
+              title : Text(name[i]),
+            );
+          }
       )
-    );
 
+      ,
+      bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Icon(Icons.phone),
+                  Icon(Icons.message),
+                  Icon(Icons.contact_page),
+                ],
+              )
+          )
+      ),
+    );
+  }
+}
+
+class DialogUI extends StatelessWidget {
+  DialogUI({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        width: 300,
+        height: 300,
+        child: Column(
+          children: [
+            TextField(),
+            TextButton( child: Text('완료'), onPressed:(){} ),
+            TextButton(
+                child: Text('취소'),
+                onPressed:(){ Navigator.pop(context); })
+          ],
+        ),
+      ),
+    );
   }
 }
